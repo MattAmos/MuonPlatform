ee45cc15f95eeb004024ee131120158cd5972f91
MattAmos <matthewjamesamos@gmail.com>
2017-08-04 19:57:33 +1000

    Create initial networking function

--- src/Runner.cpp
+++ src/Runner.cpp
@@ -3,3 +3,7 @@
 int main(int argc, char** argv) {
     return 0;
 }
+
+int scp(std::string filename) {
+    return system(("scp " + IMG_DIR + "/" + filename + " " + USER + "@" + HOST + ":/home/IMG").c_str());
+}
--- src/Runner.h
+++ src/Runner.h
@@ -1,10 +1,21 @@
+#include <eigen3/Eigen/Core>
+#include <opencv2/opencv.hpp>
+#include "../include/opencv2/highgui/highgui.hpp"
+#include "../include/opencv2/imgproc/imgproc.hpp"
+
 #include <stdio.h>
 #include <cmath>
 #include <cstring>
+#include <fstream>
 
 #ifndef RUNNER
 #define RUNNER
 
 // Store all config values and other important global variables here
+
+// Networking config parameters
+std::string IMG_DIR = "~/images/";
+std::string USER    = "matthew";
+std::string HOST    = "10.1.1.13";
 
 #endif
