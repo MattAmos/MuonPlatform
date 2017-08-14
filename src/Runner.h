2bf6cfe78b6aa6ad7e58d9876caaa40e5b61942a
MattAmos <matthewjamesamos@gmail.com>
2017-08-14 14:25:40 +1000

    Create servo delay test

--- src/Runner.h
+++ src/Runner.h
@@ -13,27 +13,16 @@
 #ifndef RUNNER
 #define RUNNER
 
-enum ServoID {
-    FL_DRIVE = 0,  // Front-left servo  for motion forwards/backwards
-    FL_YAW   = 1,  //                   for turning wheel axis
-    FR_DRIVE = 2,  // Front-right servo for motion forwards/backwards
-    FR_YAW   = 3,  //                   for turning wheel axis
-    BL_DRIVE = 4,  // Back-left servo   for motion forwards/backwards
-    BL_YAW   = 5,  //                   for turning wheel axis
-    BR_DRIVE = 6,  // Back-right servo  for motion forwards/backwards
-    BR_YAW   = 7   //                   for turning wheel axis
-};
-
 struct Servo {
-    ServoID id;  // Details role of servo
+    uint id;     // Details role of servo
     GPIO* gpio;  // Points to gpio port servo is on
 };
 
 // Store all config values and other important global variables here
 
 // Networking config parameters
-std::string IMG_DIR = "~/images/";  // Image directory to copy into
-std::string USER    = "matthew";    // Host username to copy into
-std::string HOST    = "10.1.1.13";  // Host IP to copy over network
+std::string IMG_DIR = "~/images/";   // Image directory to copy into
+std::string USER    = "matthew";     // Host username to copy into
+std::string HOST    = "10.0.0.117";  // Host IP to copy over network
 
 #endif
--- test/GPIO_test.cpp
+++ test/GPIO_test.cpp
@@ -1,11 +1,31 @@
+#include <time.h>
 #include "../src/Runner.h"
 
 int main(int argc, char** argv) {
-    Servo test;
-    test.id   = FL_DRIVE;
-    test.gpio = new GPIO("3");
+    int usec = 900;
 
-    std::cout << (test.id) << (test.gpio)->getGPIONum() << std::endl;
+    struct timespec tim;
+    tim.tv_sec  = 0;
+    tim.tv_nsec = 1000 * usec;
+
+    bool output = true;
+
+    Servo test;
+    test.id   = 16;
+    test.gpio = new GPIO("16");
+
+    test.gpio->setDirGPIO("out");
+    test.gpio->setValGPIO("1");
+
+    while (true) {
+        if (nanosleep(&tim, NULL) < 0) {
+            break;
+        }
+        output = !output;
+        test.gpio->setValGPIO(std::to_string(output));
+    }
+
+    // std::cout << (test.id) << (test.gpio)->getGPIONum() << std::endl;
 
     return 0;
 }
