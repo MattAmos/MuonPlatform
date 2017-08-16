9a956531b0981d75fb218ab01fa042de5d56c4db
MattAmos <matthewjamesamos@gmail.com>
2017-08-16 11:38:30 +1000

    Create PWM servo control for keyboard and controller inputs

--- test/GPIO_test.cpp
+++ test/GPIO_test.cpp
@@ -1,42 +1,65 @@
 #include <time.h>
+#include "../include/joystick/joystick.cc"
 #include "../src/Runner.h"
 
-int main(int argc, char** argv) {
-    // int usec = 900;
-    struct timespec rem;
-    rem.tv_sec  = 0;
-    tim.tv_nsec = 20000000;
+float getUptime(float currTime){
+    int min_t = 553, max_t = 2450;
+    int mid_t = (max_t + min_t) / 2.0;
 
-    int minTime = 1;
-    int maxTime = 5;
+    float time = (currTime == -1 ? mid_t : currTime);
 
-    bool output = true;
+    // TODO: Determine performance impact, move out of function?
+    // Create an instance of Joystick
+    Joystick joystick("/dev/input/js0");
 
-    Servo test;
-    test.id   = 12;
-    test.gpio = new GPIO("12");
-
-    test.gpio->setDirGPIO("out");
-    test.gpio->setValGPIO("1");
-
-    for (int i = minTime; i < maxTime; i++) {
-        struct timespec tim;
-        tim.tv_sec  = 0;
-        tim.tv_nsec = 1000000 * i;
-
-        while (true) {
-            if (nanosleep(&tim, NULL) < 0) {
-                break;
-            }
-            output = !output;
-            test.gpio->setValGPIO(std::to_string(output));
-            if (nanosleep(&rem, NULL) < 0) {
-                break;
+    if(joystick.isFound()){
+        JoystickEvent event;
+        if (joystick.sample(&event) && event.isAxis()) {
+            if (event.number == 0) {  // L3 HORIZONTAL
+                time = mid_t + (event.value / 65535.0) * (max_t - min_t);
             }
         }
     }
+    else{
+        int ch = getch();
+        switch(ch){
+            case 'a':
+                time -= 100;
+            break;
+            case 'd':
+                time += 100;
+            break;
+        }
+        if(time < min_t){ time = min_t; }
+        if(time > max_t){ time = max_t; }
+        move(0,0);
+        addstr(("Uptime: " + std::to_string(time)).c_str());
+        refresh();
+    }
+    return time;
+}
 
-    // std::cout << (test.id) << (test.gpio)->getGPIONum() << std::endl;
+int main(int argc, char** argv) {
+    // Setup curses terminal
+    initscr();
+    cbreak();
+    noecho();
+    keypad(stdscr, TRUE);
+
+    // Setup GPIO interface
+    GPIO test = GPIO("12");
+    test.setDirGPIO("out");
+    test.setValGPIO("0");
+
+    float time = -1;
+    while (true) {
+        time = getUptime(time);
+
+        test.setValGPIO("1");
+        usleep(time);
+        test.setValGPIO("0");
+        usleep(20000 - time);
+    }
 
     return 0;
 }
