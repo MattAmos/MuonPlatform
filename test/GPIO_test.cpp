52ed9c28fc0e8b5274b513ed344881276e60549e
MattAmos <matthewjamesamos@gmail.com>
2017-08-16 14:42:59 +1000

    Integrate controller and keyboard support for gpio test

--- test/GPIO_test.cpp
+++ test/GPIO_test.cpp
@@ -1,19 +1,18 @@
-#include <time.h>
 #include <curses.h>
+#include <time.h>
 #include "../include/joystick/joystick.cc"
 #include "../src/Runner.h"
 
-float getUptime(float currTime){
-    int min_t = 553, max_t = 2450;
-    int mid_t = (max_t + min_t) / 2.0;
+int min_t = 553, max_t = 2450;
+int mid_t = (max_t + min_t) / 2.0;
 
+// Create an instance of Joystick
+Joystick joystick("/dev/input/js0");
+
+float getUptime(float currTime) {
     float time = (currTime == -1 ? mid_t : currTime);
 
-    // TODO: Determine performance impact, move out of function?
-    // Create an instance of Joystick
-    Joystick joystick("/dev/input/js0");
-
-    if(joystick.isFound()){
+    if (joystick.isFound()) {
         JoystickEvent event;
         if (joystick.sample(&event) && event.isAxis()) {
             if (event.number == 0) {  // L3 HORIZONTAL
@@ -21,21 +20,18 @@ float getUptime(float currTime){
             }
         }
     }
-    else{
+    else {
         int ch = getch();
-        switch(ch){
-            case 'a':
-                time -= 100;
-            break;
-            case 'd':
-                time += 100;
-            break;
+        switch (ch) {
+            case 'a': time -= 100; break;
+            case 'd': time += 100; break;
         }
-        if(time < min_t){ time = min_t; }
-        if(time > max_t){ time = max_t; }
-        move(0,0);
-        addstr(("Uptime: " + std::to_string(time)).c_str());
-        refresh();
+        if (time < min_t) {
+            time = min_t;
+        }
+        if (time > max_t) {
+            time = max_t;
+        }
     }
     return time;
 }
@@ -55,6 +51,10 @@ int main(int argc, char** argv) {
     float time = -1;
     while (true) {
         time = getUptime(time);
+
+        move(0, 0);
+        addstr(("Duty Cycle: " + std::to_string(time) + " (us)").c_str());
+        refresh();
 
         test.setValGPIO("1");
         usleep(time);
