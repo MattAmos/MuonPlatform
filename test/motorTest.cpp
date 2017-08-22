ea58991e58e2c362df8f2040b33991955acc744d
MattAmos <matthewjamesamos@gmail.com>
2017-08-22 13:03:30 +1000

    Motor test tests pigpio library

--- test/motorTest.cpp
+++ test/motorTest.cpp
@@ -1,7 +1,15 @@
-#include <time.h>
-#include "../src/GPIO.cpp"
+#include <stdio.h>
+#include "../include/pigpio.h"
+
 /*
+   This code may be used to drive the Adafruit (or clones) Motor Shield.
+
+   The code as written only supports DC motors.
+
+   http://shieldlist.org/adafruit/motor
+
    The shield pinouts are
+
    D12 MOTORLATCH
    D11 PMW motor 1
    D10 Servo 1
@@ -17,19 +25,38 @@
    The motor states (forward, backward, brake, release) are encoded using the
    MOTOR_ latch pins.  This saves four gpios.
 */
+
 typedef unsigned char uint8_t;
+
 #define BIT(bit) (1 << (bit))
 
-// Define pins to drive shield pins
+/* assign gpios to drive the shield pins */
+
+/*      Shield      Pi */
+
 #define MOTORLATCH 14
 #define MOTORCLK 24
 #define MOTORENABLE 25
 #define MOTORDATA 15
 
-#define MOTOR_1_PWM 7
+#define MOTOR_3_PWM 7
+#define MOTOR_4_PWM 8
+
+/*
+   The only other connection needed between the Pi and the shield
+   is ground to ground. I used Pi P1-6 to shield gnd (next to D13).
+*/
+
+/* assignment of motor states to latch */
 
 #define MOTOR1_A 2
 #define MOTOR1_B 3
+#define MOTOR2_A 1
+#define MOTOR2_B 4
+#define MOTOR4_A 0
+#define MOTOR4_B 6
+#define MOTOR3_A 5
+#define MOTOR3_B 7
 
 #define FORWARD 1
 #define BACKWARD 2
@@ -38,45 +65,50 @@ typedef unsigned char uint8_t;
 
 static uint8_t latch_state;
 
-GPIO latch("14"), clk("24"), enable("25"), data("15"), pwm("7");
-
-void latch_tx() {
+void latch_tx(void) {
     unsigned char i;
 
-    latch.setValGPIO("0");
+    gpioWrite(MOTORLATCH, PI_LOW);
 
-    data.setValGPIO("0");
+    gpioWrite(MOTORDATA, PI_LOW);
 
     for (i = 0; i < 8; i++) {
-        usleep(10);
-        clk.setValGPIO("0");
+        gpioDelay(10);  // 10 micros delay
 
-        if (latch_state & BIT(7 - i)) {
-            data.setValGPIO("1");
-        }
-        else {
-            data.setValGPIO("0");
-        }
+        gpioWrite(MOTORCLK, PI_LOW);
 
-        usleep(10);
+        if (latch_state & BIT(7 - i))
+            gpioWrite(MOTORDATA, PI_HIGH);
+        else
+            gpioWrite(MOTORDATA, PI_LOW);
 
-        clk.setValGPIO("1");
+        gpioDelay(10);  // 10 micros delay
+
+        gpioWrite(MOTORCLK, PI_HIGH);
     }
-    latch.setValGPIO("1");
+
+    gpioWrite(MOTORLATCH, PI_HIGH);
 }
 
-void init() {
+void init(void) {
     latch_state = 0;
+
     latch_tx();
-    enable.setValGPIO("0");
+
+    gpioWrite(MOTORENABLE, PI_LOW);
 }
 
 void DCMotorInit(uint8_t num) {
     switch (num) {
         case 1: latch_state &= ~BIT(MOTOR1_A) & ~BIT(MOTOR1_B); break;
+        case 2: latch_state &= ~BIT(MOTOR2_A) & ~BIT(MOTOR2_B); break;
+        case 3: latch_state &= ~BIT(MOTOR3_A) & ~BIT(MOTOR3_B); break;
+        case 4: latch_state &= ~BIT(MOTOR4_A) & ~BIT(MOTOR4_B); break;
         default: return;
     }
+
     latch_tx();
+
     printf("Latch=%08X\n", latch_state);
 }
 
@@ -87,6 +119,18 @@ void DCMotorRun(uint8_t motornum, uint8_t cmd) {
         case 1:
             a = MOTOR1_A;
             b = MOTOR1_B;
+            break;
+        case 2:
+            a = MOTOR2_A;
+            b = MOTOR2_B;
+            break;
+        case 3:
+            a = MOTOR3_A;
+            b = MOTOR3_B;
+            break;
+        case 4:
+            a = MOTOR4_A;
+            b = MOTOR4_B;
             break;
         default: return;
     }
@@ -112,46 +156,57 @@ void DCMotorRun(uint8_t motornum, uint8_t cmd) {
     printf("Latch=%08X\n", latch_state);
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char* argv[]) {
     int i;
 
-    latch.setDirGPIO("out");
-    clk.setDirGPIO("out");
-    enable.setDirGPIO("out");
-    data.setDirGPIO("out");
-    pwm.setDirGPIO("out");
+    if (gpioInitialise() < 0) return 1;
 
-    pwm.setValGPIO("1");
-    usleep(128);
-    pwm.setValGPIO("0");
-    usleep(255 - 128);
+    gpioSetMode(MOTORLATCH, PI_OUTPUT);
+    gpioSetMode(MOTORENABLE, PI_OUTPUT);
+    gpioSetMode(MOTORDATA, PI_OUTPUT);
+    gpioSetMode(MOTORCLK, PI_OUTPUT);
+
+    gpioSetMode(MOTOR_3_PWM, PI_OUTPUT);
+    gpioSetMode(MOTOR_4_PWM, PI_OUTPUT);
+
+    gpioPWM(MOTOR_3_PWM, 0);
+    gpioPWM(MOTOR_4_PWM, 0);
 
     init();
 
-    DCMotorRun(1, FORWARD);
+    for (i = 60; i < 160; i += 20) {
+        gpioPWM(MOTOR_3_PWM, i);
+        gpioPWM(MOTOR_4_PWM, 220 - i);
 
-    // for (i = 60; i < 160; i += 20) {
-    //     gpioPWM(pwm, i);
+        DCMotorRun(3, FORWARD);
+        DCMotorRun(4, BACKWARD);
 
-    //     DCMotorRun(1, FORWARD, latch, data, clk);
+        sleep(2);
 
-    //     sleep(2);
+        DCMotorRun(3, RELEASE);
+        DCMotorRun(4, RELEASE);
 
-    //     DCMotorRun(1, RELEASE, latch, data, clk);
+        sleep(2);
 
-    //     sleep(2);
+        gpioPWM(MOTOR_4_PWM, i);
+        gpioPWM(MOTOR_3_PWM, 220 - i);
 
-    //     gpioPWM(pwm, 220 - i);
+        DCMotorRun(3, BACKWARD);
+        DCMotorRun(4, FORWARD);
 
-    //     DCMotorRun(1, BACKWARD, latch, data, clk);
+        sleep(2);
 
-    //     sleep(2);
+        DCMotorRun(3, RELEASE);
+        DCMotorRun(4, RELEASE);
 
-    //     DCMotorRun(1, RELEASE, latch, data, clk);
-    //     sleep(2);
-    // }
-    pwm.setValGPIO("0");
-    DCMotorRun(1, RELEASE);
+        sleep(2);
+    }
 
-    return 0;
+    gpioPWM(MOTOR_4_PWM, 0);
+    gpioPWM(MOTOR_3_PWM, 0);
+
+    DCMotorRun(3, RELEASE);
+    DCMotorRun(4, RELEASE);
+
+    gpioTerminate();
 }
