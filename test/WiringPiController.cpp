ba53efc23715654c40d3fb0bfeced8e6741727e2
MattAmos <matthewjamesamos@gmail.com>
2017-08-23 11:24:14 +1000

    Create wiringPi servo test

---
+++ test/WiringPiController.cpp
@@ -0,0 +1,47 @@
+#include <curses.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <iostream>
+#include <string>
+
+#include <softPwm.h>
+#include <wiringPi.h>
+#include "../src/PinMap.h"
+
+#define MIN_T 600.0
+#define MAX_T 1700.0
+
+int main(int argc, char** argv) {
+    if (wiringPiSetupGpio() < 0) {
+        std::cout << "Error setting up wiringPi. Exiting..." << std::endl;
+        return -1;
+    }
+
+    if (softPwmCreate(SERVO, 1100, 100) != 0) {
+        std::cout << "Error setting up pin" << SERVO << ". Exiting..." << std::endl;
+        return -1;
+    }
+
+    float time = (MAX_T + MIN_T) / 2.0;
+
+    while (true) {
+        int ch = getch();
+        switch (ch) {
+            case 'w': break;
+            case 'a': time -= 100; break;
+            case 's': break;
+            case 'd': time += 100; break;
+        }
+        if (time < MIN_T) {
+            time = MIN_T;
+        }
+        else if (time > MAX_T) {
+            time = MAX_T;
+        }
+        move(0, 0);
+        addstr(("Uptime: " + std::to_string(time)).c_str());
+        refresh();
+
+        softPwmWrite(SERVO, int(time));
+    }
+}
