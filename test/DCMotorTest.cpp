38acc9cc85fc4be6b6a3a7ddfb363ca71691aba1
MattAmos <matthewjamesamos@gmail.com>
2017-09-05 10:52:48 +1000

    Updat dc motor test

--- test/DCMotorTest.cpp
+++ test/DCMotorTest.cpp
@@ -5,6 +5,9 @@
 #include "../src/GPIO.cpp"
 #include "../src/PinMap.h"
 
+int min_t = SERVO_PWM_MIN, max_t = SERVO_PWM_MAX;
+int mid_t = (max_t + min_t) / 2.0;
+
 GPIO dc_a = GPIO(std::to_string(DC_A));
 GPIO dc_b = GPIO(std::to_string(DC_B));
 GPIO dc_s = GPIO(std::to_string(DC_S));
@@ -15,10 +18,34 @@ JoystickEvent event;
 
 void signalHandler(int sigNum) {
     std::cout << "Interrupt signal (" << sigNum << ") received. Exiting gracefully..." << std::endl;
-    dc_a.~GPIO();
-    dc_b.~GPIO();
-    dc_s.~GPIO();
+    test.~GPIO();
     exit(sigNum);
+}
+
+float getUptime(float currTime) {
+    float time = (currTime == -1 ? mid_t : currTime);
+
+    if (joystick.isFound()) {
+        if (joystick.sample(&event) && event.isAxis()) {
+            if (event.number == 0) {  // L3 HORIZONTAL
+                time = mid_t + (event.value / 65535.0) * (max_t - min_t);
+            }
+        }
+    }
+    else {
+        int ch = getch();
+        switch (ch) {
+            case 'a': time += 100; break;
+            case 'd': time -= 100; break;
+        }
+        if (time < min_t) {
+            time = min_t;
+        }
+        if (time > max_t) {
+            time = max_t;
+        }
+    }
+    return time;
 }
 
 int main(int argc, char** argv) {
@@ -30,6 +57,26 @@ int main(int argc, char** argv) {
     dc_a.setValGPIO("0");
     dc_b.setDirGPIO("out");
     dc_b.setValGPIO("1");
+
+    // Setup curses terminal
+    initscr();
+    cbreak();
+    noecho();
+    keypad(stdscr, TRUE);
+
+    float time = -1;
+    while (true) {
+        time = getUptime(time);
+
+        move(0, 0);
+        addstr(("Duty Cycle: " + std::to_string(time) + " (us)").c_str());
+        refresh();
+
+        dc_b.setValGPIO("1");
+        usleep(time);
+        dc_b.setValGPIO("0");
+        usleep(20000 - time);
+    }
 
     return 0;
 }
