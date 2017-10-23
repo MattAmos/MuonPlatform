77072b993fcc0f8159cf38af74ac4f3cf31c4cb4
Matthew Amos <matthewjamesamos@gmail.com>
2017-10-23 08:09:55 +0000

    Tune person detector parameters

--- test/CVTest1.cpp
+++ test/CVTest1.cpp
@@ -1,40 +1,45 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <raspicam/raspicam_cv.h>
  
 using namespace std;
 using namespace cv;
  
 int main (int argc, const char * argv[])
 {
-    VideoCapture cap(0);
-    if (!cap.isOpened()){
+    raspicam::RaspiCam_Cv Camera;
+
+    Camera.set(CV_CAP_PROP_FRAME_WIDTH, 320);
+    Camera.set(CV_CAP_PROP_FRAME_HEIGHT, 240);  
+
+    if (!Camera.open()){
         std::cout << "Unable to open video capture. Exiting..." << std::endl;
         return -1;
     }
     
-    cap.set(CV_CAP_PROP_FRAME_WIDTH, 320);
-    cap.set(CV_CAP_PROP_FRAME_HEIGHT, 240);    
- 
     Mat img, img_gray;
     HOGDescriptor hog;
     hog.setSVMDetector(HOGDescriptor::getDefaultPeopleDetector());
- 
+    
     namedWindow("video capture", CV_WINDOW_AUTOSIZE);
     while (true)
     {
-        cap >> img;
+	Camera.grab();
+        Camera.retrieve(img);
+
         if (!img.data){
             std::cout << "No image data. Continuing..." << std::endl;
-            continue;
+            break;
         }
         
         vector<Rect> found, found_filtered;
         cvtColor(img, img_gray, CV_BGR2GRAY);
-        equalizeHist(img_gray, img_gray);
-        hog.detectMultiScale(img_gray, found, 0, Size(8,8), Size(32,32), 1.05, 2);
+        //equalizeHist(img_gray, img_gray);
+        hog.detectMultiScale(img_gray, found, 0, Size(4,4), Size(0,0), 1.08, 3);
 
         size_t i, j;
-        for (i=0; i<found.size(); i++){
+        /*
+	for (i=0; i<found.size(); i++){
             Rect r = found[i];
             for (j=0; j<found.size(); j++){
                 if (j!=i && (r & found[j]) == r){
@@ -46,8 +51,9 @@ int main (int argc, const char * argv[])
                 found_filtered.push_back(r);
             }
         }
-        for (i=0; i<found_filtered.size(); i++){
-            Rect r = found_filtered[i];
+	*/
+        for (i=0; i<found.size(); i++){
+            Rect r = found[i];
             r.x += cvRound(r.width*0.1);
             r.width = cvRound(r.width*0.8);
             r.y += cvRound(r.height*0.06);
@@ -61,5 +67,6 @@ int main (int argc, const char * argv[])
             break;
         }
     }
+    Camera.release();
     return 0;
 }
